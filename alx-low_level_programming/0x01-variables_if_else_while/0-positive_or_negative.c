6038677143cf8960d85c18aa4d7b1e6d7d08f9fd
gachafrejus <frejuskinf@gmail.com>
2022-12-11 05:12:38 -0800

    task0

--- alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c
+++ alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,24 +3,25 @@
 #include <time.h>
 
 /**
- * main -main function to generate a randon number
+ * main - main function to generate a randon number
  *
  * Return: Always 0 (success)
  *
  */
 
-int main (void)
+int main(void)
 {
 	int n;
+
 	srand(time(0));
-	n = rand() -RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
-	{	
+	{
 		printf("%d is zero\n", n);
 	}
 	else if (n < 0)
