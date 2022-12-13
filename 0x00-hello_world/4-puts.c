38873cd950808eb17a04270eedeeac52ae6fa7cc
gachafrejus <frejuskinf@gmail.com>
2022-12-12 19:43:21 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - print a line of code using puts
- * Return: 0
+ * main - prints a given phrase
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
- int main(void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	  puts("\"Programming is like building a multilingual puzzle");                   return (0);
 }
- 
- 
