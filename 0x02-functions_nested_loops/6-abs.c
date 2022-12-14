3a7f8e07ee4ee058729de882bd1d5a869113f786
gachafrejus <frejuskinf@gmail.com>
2022-12-14 14:14:22 -0800

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,10 +4,10 @@
  * _abs - computes the abs value of an int
  * @n: the int in question
  *
- * Return: the abs of the int.
+ * Return: the abs of the int
  */
 
-int _abs(int);
+int _abs(int n);
 {
 	if (n >= 0)
 		return (n);
